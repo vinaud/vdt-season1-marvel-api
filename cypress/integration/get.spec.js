7183707aaba45e07ee1de94e28c1b86dda454718
Vinaud <leyenasd@gmail.com>
2022-06-23 14:42:37 -0300

    Test: Search character by id

--- cypress/integration/get.spec.js
+++ cypress/integration/get.spec.js
@@ -50,4 +50,37 @@ describe('GET /characters', function(){
         });
 
     });
-})
+});
+
+describe('GET /characters/id', function(){
+
+    before(function(){
+
+        cy.back2ThePast();
+        cy.setToken();
+    });
+
+    const tonyStark = {
+        name: "Tony Stark",
+        alias: "Homem de Ferro",
+        team: ['Vingadores'],
+        active: true
+    }
+
+    context('quando tem um personagem cadastrado', function(){
+        before(function(){
+            cy.postCharacter(tonyStark).then(function(response){
+                Cypress.env('characterId', response.body.character_id)
+            })
+        });
+        it('deve buscar o personagem pelo id', function(){
+            const id = Cypress.env('characterId');
+            cy.getCharacterById(id).then(function(response){
+                expect(response.status).to.be.eql(200);
+                expect(response.body.alias).to.be.eql('Homem de Ferro');
+                expect(response.body.team).to.be.eql(['Vingadores']);
+                expect(response.body.active).to.be.eql(true);
+            })
+        });
+    });
+});
